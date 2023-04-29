364ad97223dd03fe432550baf1282c20ce68a02b
hkdark <33818840+khanh97dev@users.noreply.github.com>
2023-04-29 03:47:09 +0000

    Added: add vite to npm

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import {
+    quasar,
+    transformAssetUrls
+} from '@quasar/vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls
+            },
+        }),
+
+        quasar({
+            sassVariables: 'src/quasar-variables.sass'
+        })
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
